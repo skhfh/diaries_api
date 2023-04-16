b84d6f85f49cfbab74c132470dd67150579f1023
Шамиль Хамидуллин <shamil.415@yandex.ru>
2023-04-16 14:10:53 +0300

    Send to the first review

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -10,6 +10,6 @@ class ReadOnlyOrAuthor(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return (
-                request.method in permissions.SAFE_METHODS
-                or obj.author == request.user
+            request.method in permissions.SAFE_METHODS
+            or obj.author == request.user
         )
